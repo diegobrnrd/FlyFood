550e7b1e6dc638b2122a5e60f92ca8aa2543a9eb
Diego Bernardo <diegobsiufrpe@gmail.com>
2024-07-19 05:50:34 -0300

    Update flyfood_forca_bruta.py

--- força-bruta/flyfood_forca_bruta.py
+++ força-bruta/flyfood_forca_bruta.py
@@ -1,28 +1,16 @@
-# Registra a entrada em um arquivo e cria as variáveis linha e coluna no escopo global.
-def entrada():
-    with open('dados.txt', 'w') as arquivo:
-        primeira_linha = input()
-        arquivo.write(primeira_linha)
-        lin, col = primeira_linha.split()
-
-        for _ in range(int(lin)):
-            arquivo.write(f'\n{input()}')
-
-        return int(lin), int(col)
-
-
-# Ler o arquivo e cria no escopo global uma matriz em formato de lista
-def ler_dados(lin):
-    matr = [[] for _ in range(lin)]
+# Coleta a quantidade de linha(s), coluna(s) e a matriz armazenada em uma lista
+def abrir_arquivo():
     with open('dados.txt', 'r') as arquivo:
-        arquivo.readline()
+        lin, col = arquivo.readline().split()
+        matr = [[] for _ in range(int(lin))]
         for i, linha_dados in enumerate(arquivo):
             matr[i] = linha_dados.split()
 
-    return matr
+    return int(lin), int(col), matr
 
 
 # Converte a matriz em dicionário com suas respectivas coordenadas
+# Dois dicionários são criados, um sem o ponto R e o outro completo
 def obter_coordenadas(matr):
     coord_1, coord_2 = {}, {}
 
@@ -38,8 +26,10 @@ def obter_coordenadas(matr):
     return coord_1, coord_2
 
 
-# Obtém todas as rotas possíveis
-def obter_rotas(cord):
+# Gera todas as rotas possíveis
+def gerar_rotas(cord):
+    import itertools
+
     pontos = [None for _ in range(len(cord))]
     for i, p in enumerate(cord):
         pontos[i] = p
@@ -50,55 +40,62 @@ def obter_rotas(cord):
     return permutacoes_com_r
 
 
-# Cálcula a distância de todas as rotas
-def calcular_rotas(coord, rots):
-    resultado_final = {x: -1 for x in rots}
+# Cálcula a distância de todas as rotas | Função 1-3
+def calcular_rotas(coord, rotass):
+    resultado_final = {x: -1 for x in rotass}
 
-    for rota_atual in rots:
-        resultado_final[rota_atual] = somar_distancias(coord, rota_atual)
+    for rota_atual in rotass:
+        resultado_final[rota_atual] = calcular_rota(coord, rota_atual)
 
     return resultado_final
 
 
-# Cálcula a distância de uma rota
-def somar_distancias(coord, rota_atual):
+# Cálcula a distância de uma rota | Função 2-3
+def calcular_rota(coord, rota_atual):
     soma = 0
+
     for i in range(len(rota_atual) - 1):
         soma += calcular_distancia(coord[rota_atual[i]], coord[rota_atual[i + 1]])
 
     return soma
 
 
-# Cálcula a distância entre dois pontos
+# Cálcula a distância entre dois pontos | Função 3-3
 def calcular_distancia(ponto_1, ponto_2):
+
     return abs(ponto_2[0] - ponto_1[0]) + abs(ponto_2[1] - ponto_1[1])
 
 
-# Encontra qual o menor percurso e sua distância
+# Encontra qual o menor percurso e sua distância em dronômetros
 def obter_menor_percurso(dist):
-    chave, valor, menor = '', -1, float('inf')
-    for k, v in dist.items():
-        if v < menor:
-            chave = k
-            valor = v
-            menor = v
+    chave, menor = '', float('inf')
 
-    return chave, valor
+    for chav, valor in dist.items():
+        if valor < menor:
+            chave = chav
+            menor = valor
+
+    return chave, menor
 
 
 # Formata a saída
 def saida(chave, valor):
     rota = [x for x in chave if x != 'R']
-    s = f'Rota: {" ".join(rota)} | Percurso: {valor} dronômetros'
-    return s
+    saida_formatada = f'Rota: {" ".join(rota)} | Percurso: {valor} dronômetros'
+    return saida_formatada
 
 
-if __name__ == '__main__':
-    linha, coluna = entrada()
-    matriz = ler_dados(linha)
+# Função principal que chama todas as outras
+def funcao_principal():
+    linha, coluna, matriz = abrir_arquivo()
     coordenadas_sem_r, coordenadas_com_r = obter_coordenadas(matriz)
-    import itertools
-    rotas = obter_rotas(coordenadas_sem_r)
+    rotas = gerar_rotas(coordenadas_sem_r)
     distancias = calcular_rotas(coordenadas_com_r, rotas)
     menor_percurso, menor_distancia = obter_menor_percurso(distancias)
     print(saida(menor_percurso, menor_distancia))
+
+
+if __name__ == '__main__':
+    import timeit
+    tempo_de_execucao = timeit.timeit(funcao_principal, number=1)
+    print(f"Tempo de execução: {tempo_de_execucao} segundos")

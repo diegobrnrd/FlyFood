83fd71e3bc5d7a47b9404a37fcd0617ecee3d167
Diego Bernardo <diegobsiufrpe@gmail.com>
2024-07-26 09:07:42 -0300

    Update flyfood_forca_bruta.py

--- força-bruta/flyfood_forca_bruta.py
+++ força-bruta/flyfood_forca_bruta.py
@@ -4,36 +4,36 @@
 def abrir_arquivo():
     """Obtém a quantidade de linhas e colunas, além da matriz armazenada em uma lista."""
     with open('dados.txt', 'r') as arquivo:
-        lin, col = arquivo.readline().split()
-        matr = [[] for _ in range(int(lin))]
+        linha, coluna = arquivo.readline().split()
+        matriz = [[] for _ in range(int(linha))]
         for i, linha_dados in enumerate(arquivo):
-            matr[i] = linha_dados.split()
+            matriz[i] = linha_dados.split()
 
-    return int(lin), int(col), matr
+    return int(linha), int(coluna), matriz
 
 
-def obter_coordenadas(matr):
+def obter_coordenadas(matriz):
     """Coleta as coordenadas dos pontos de interesse e as armazena em um dicionário."""
-    coord_1, coord_2 = {}, {}
-    # Dois dicionários são criados: um sem o ponto R e o outro completo.
-    for x, l in enumerate(matr):
+    coordenadas_com_r = {}
+    # Dicionário com o ponto R.
+    for x, l in enumerate(matriz):
         for y, c in enumerate(l):
-            if c == 'A' or c == 'B' or c == 'C' or c == 'D':
-                coord_1[f'{c}'] = (int(x), int(y))
-            elif c == 'R':
-                coord_2[f'{c}'] = (int(x), int(y))
+            if c != '0':
+                coordenadas_com_r[f'{c}'] = (int(x), int(y))
 
-    coord_2.update(coord_1)
+    coordenadas_sem_r = coordenadas_com_r.copy()
+    del coordenadas_sem_r['R']
+    # Dicionário sem o ponto R.
 
-    return coord_1, coord_2
+    return coordenadas_com_r, coordenadas_sem_r
 
 
-def gerar_rotas(coord):
+def gerar_rotas(coordenadas):
     """Gera todas as combinações possíveis de rotas."""
     import itertools
 
-    pontos = [None for _ in range(len(coord))]
-    for i, p in enumerate(coord):
+    pontos = [None for _ in range(len(coordenadas))]
+    for i, p in enumerate(coordenadas):
         pontos[i] = p
 
     permutacoes = list(itertools.permutations(pontos))
@@ -44,22 +44,23 @@ def gerar_rotas(coord):
     return permutacoes_com_r
 
 
-def calcular_rotas(coord, rotass):
+def calcular_rotas(coordenadas, rotas):
     """Calcula a distância total para cada uma das rotas possíveis."""
-    resultado_final = {x: -1 for x in rotass}
+    distancias = {x: -1 for x in rotas}
     # Inicialmente, as distâncias são inicializadas com o valor -1.
-    for rota_atual in rotass:
-        resultado_final[rota_atual] = calcular_rota(coord, rota_atual)
-        # A rota será a chave e a distância será o valor associado.
+    # As rotas são as chaves.
+    for rota_atual in rotas:
+        distancias[rota_atual] = calcular_rota(coordenadas, rota_atual)
+        # É atribuído o valor de cada rota atual.
 
-    return resultado_final
+    return distancias
 
 
-def calcular_rota(coord, rota_atual):
+def calcular_rota(coordenadas, rota_atual):
     """Calcula a distância total de uma rota específica."""
     soma = 0
     for i in range(len(rota_atual) - 1):
-        soma += calcular_distancia(coord[rota_atual[i]], coord[rota_atual[i + 1]])
+        soma += calcular_distancia(coordenadas[rota_atual[i]], coordenadas[rota_atual[i + 1]])
 
     return soma
 
@@ -69,36 +70,37 @@ def calcular_distancia(ponto_1, ponto_2):
     return abs(ponto_2[0] - ponto_1[0]) + abs(ponto_2[1] - ponto_1[1])
 
 
-def obter_menor_percurso(dist):
+def obter_menor_percurso(distancias):
     """Encontra o menor percurso e sua distância em dronômetros."""
-    chave, menor = '', float('inf')
+    percurso, menor_distancia = '', float('inf')
 
-    for chav, valor in dist.items():
-        if valor < menor:
-            chave = chav
-            menor = valor
+    for chave, valor in distancias.items():
+        if valor < menor_distancia:
+            percurso = chave
+            menor_distancia = valor
 
-    return chave, menor
+    return percurso, menor_distancia
 
 
 def saida(chave, valor):
     """Formata a saída de dados."""
     rota = [x for x in chave if x != 'R']
+    # O ponto R inicial e final são retirados para exibição do resultado.
     saida_formatada = f'Rota: {" ".join(rota)} - Percurso: {valor} dronômetros'
     return saida_formatada
 
 
-def funcao_principal():
+def central():
     """Função principal que gerencia a chamada de todas as outras funções."""
     linha, coluna, matriz = abrir_arquivo()
-    coordenadas_sem_r, coordenadas_com_r = obter_coordenadas(matriz)
+    coordenadas_com_r, coordenadas_sem_r = obter_coordenadas(matriz)
     rotas = gerar_rotas(coordenadas_sem_r)
     distancias = calcular_rotas(coordenadas_com_r, rotas)
-    menor_percurso, menor_distancia = obter_menor_percurso(distancias)
-    print(saida(menor_percurso, menor_distancia))
+    percurso, menor_distancia = obter_menor_percurso(distancias)
+    print(saida(percurso, menor_distancia))
 
 
 if __name__ == '__main__':
     import timeit
-    tempo_de_execucao = timeit.timeit(funcao_principal, number=1)
+    tempo_de_execucao = timeit.timeit(central, number=1)
     print(f'Tempo de execução: {tempo_de_execucao} segundos')
